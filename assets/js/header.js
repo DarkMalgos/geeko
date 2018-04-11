8a29fc71713f14d490c256caf79e95853daf0941
Florian Saumurot <florian.saumurot@gmail.com>
2018-04-11 23:34:50 +0200

    connexion & inscription done

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,10 +3,14 @@ document.querySelector('#menu .burger').addEventListener('click', function () {
     if (this.querySelector('.bar').getAttribute('class') == 'bar') {
         this.querySelector('.bar').classList.add('active')
         document.querySelector('#menu nav').style.display = 'flex'
+        document.querySelector('#menu').style.height = '100vh'
+        this.style.justifyContent = 'flex-start'
         document.querySelector('main section').style.display = 'none'
     } else {
         this.querySelector('.bar').classList.remove('active')
         document.querySelector('#menu nav').style.display = 'none'
         document.querySelector('main section').style.display = 'flex'
+        document.querySelector('#menu').style.height = '10vh'
+        this.style.justifyContent = 'space-around'
     }
 })
